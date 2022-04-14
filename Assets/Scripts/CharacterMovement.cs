1286439e64404a675fcfbd98a66802cf1acc3c68
IoniBini <ionimaghidman@gmail.com>
2022-04-15 01:26:04 +1000

    Attack + Shield Animations
    Implemented all attacks into the animation scheme + shield animations + fixed small bug where the buffer timer would infinitely go down

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    //movement variables
     protected Rigidbody playerController;
     protected FloorChecker floorCheckerBox;
     public float speed = 10f;
     public Vector3 movement;
+    public float slowMoveWhileAttacking = 1f;
 
+    //animation variables
     protected Animator characterAnimator;
-    public float jumpTimerReset = 0.5f;
 
+    //attack variables
+    public float resetToIdle = 0.5f;
+    private int attackPhaseCounter = 0;
+    private float attackDelayTime = 0.4f;
+    private float attackDelayCounter;
+    public bool isAttacking = false;
+
+    //jump variables
+    public float jumpTimerReset = 0.5f;
+    private bool avoidJumpSpam = false;
+    public float jumpStrenght = 5f;
     public float coyoteTime = 0.2f;
     public float coyoteTimeCounter;
-
     public float bufferTime = 0.2f;
     public float bufferTimeCounter;
-
-    private bool avoidJumpSpam = false;
-
-    public float jumpStrenght = 5f;
 
     private void Awake()
     {
@@ -50,15 +58,15 @@ public class CharacterMovement : MonoBehaviour
         {
             bufferTimeCounter = bufferTime;
         }
-        else
+        else if (bufferTimeCounter >= 0f)
         {
             bufferTimeCounter = -Time.deltaTime;
         }
 
 
-            //freezes the character in place if no input is being received, so that it doesn't slide down sloped surfaces.
-            //I do want to say I have found a specific instance where if the player tries to walk both ways at once on a sloped surface, they will fall as they are no longer kinematic. I didn't have time to fix it though...
-            if (floorCheckerBox.isGrounded == true && Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        //freezes the character in place if no input is being received, so that it doesn't slide down sloped surfaces.
+        //I do want to say I have found a specific instance where if the player tries to walk both ways at once on a sloped surface, they will fall as they are no longer kinematic. I didn't have time to fix it though...
+        if (floorCheckerBox.isGrounded == true && Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
         {
             GetComponent<Rigidbody>().isKinematic = true;
         }
@@ -127,18 +135,104 @@ public class CharacterMovement : MonoBehaviour
                 StartCoroutine(jumpReset());
             }
         }
+
+        //activate attack
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            isAttacking = true;
+
+            //if player only attacks once, then only the first animation will play, but if twice, then the second will play. This loop can be repeated without going back to idle.
+            if (attackPhaseCounter == 0)
+            {
+                //every time you are not attacking, the delay counter is returned to its original value
+                attackDelayCounter = attackDelayTime;
+                attackPhaseCounter = 1;
+            }
+            else if (attackPhaseCounter == 1)
+            {
+                attackDelayCounter = attackDelayTime;
+                attackPhaseCounter = 2;
+            }
+            else if (attackPhaseCounter == 2)
+            {
+                attackDelayCounter = attackDelayTime;
+                attackPhaseCounter = 1;
+            }
+
+            if (attackPhaseCounter == 1)
+            {
+                characterAnimator.SetBool("isAttacking1", true);
+                characterAnimator.SetBool("isAttacking2", false);
+            }
+            else if (attackPhaseCounter == 2)
+            {
+                characterAnimator.SetBool("isAttacking1", false);
+                characterAnimator.SetBool("isAttacking2", true);
+            }
+        }
+
+        //after the animtaion and the counter are resolved, the game waits for a short second. If the next input is set, then the next animation plays.
+        if (isAttacking == true)
+        {
+            attackDelayCounter -= Time.deltaTime;
+        }
+
+        if (attackDelayCounter <= 0f && isAttacking == true)
+        {
+            //But then if nothing is pressed, then it returns you to idle via the coroutine
+            StartCoroutine(attackReset());
+        }
+
+        //activate shield, can't be activated midair
+        if (Input.GetKey(KeyCode.J) && floorCheckerBox.isGrounded == true)
+        {
+            //slow player movement down gradually while defending and moving
+            if (slowMoveWhileAttacking >= 0.001f)
+            {
+                slowMoveWhileAttacking -= Time.deltaTime * 3f;
+            }
+            //once you reach the slowest speed possible, you freeze altogether, and become kinematic so that you don't slide down slopes when attacking
+            else if (slowMoveWhileAttacking <= 0.01f)
+            {
+                slowMoveWhileAttacking = 0;
+                GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+            characterAnimator.SetBool("isDefending", true);
+        }
+        else
+        {
+            //slowly but a little faster build it back up when you stop attacking
+            if (slowMoveWhileAttacking <= 1)
+            {
+                slowMoveWhileAttacking += Time.deltaTime;
+            }
+
+            characterAnimator.SetBool("isDefending", false);
+        }
     }
 
     IEnumerator jumpReset()
     {
         avoidJumpSpam = true;
+        //to avoid frustration, I removed the majority of the slowing down from the attack once you jump to dodge
+        slowMoveWhileAttacking = 0.7f;
         //the character needs at least one frame to get off the floor for the animation to commence, hence I'm making it wait before resetting the jump bool to false 
         yield return new WaitForSeconds(jumpTimerReset);
-        avoidJumpSpam = false
-            ;
+        avoidJumpSpam = false;
         characterAnimator.SetBool("isJumping", false);
         characterAnimator.SetBool("isFallingIdleRight", true);
         characterAnimator.SetBool("isFallingIdleLeft", true);
+    }
+
+    IEnumerator attackReset()
+    {
+        attackPhaseCounter = 0;
+        //the animation needs to be reset when you stop pressing the attack
+        yield return new WaitForSeconds(resetToIdle);
+        isAttacking = false;
+        characterAnimator.SetBool("isAttacking1", false);
+        characterAnimator.SetBool("isAttacking2", false);
     }
 
     private void FixedUpdate()
@@ -159,7 +253,24 @@ public class CharacterMovement : MonoBehaviour
         }
         else
         {
-            playerController.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+            //slow player movement down gradually while in attack, and also slowly but a little faster build it back up when you stop attacking
+            if (isAttacking == true && slowMoveWhileAttacking >= 0.01f)
+            {
+                slowMoveWhileAttacking -= Time.deltaTime * 1.5f;
+            }
+            //also checks to see if you are not defending before returning movement speed to normal
+            else if (slowMoveWhileAttacking <= 1 && !Input.GetKey(KeyCode.J))
+            {
+                slowMoveWhileAttacking += Time.deltaTime;
+            }
+
+            //once you reach the slowest speed possible, you freeze altogether, so that you don't slide down slopes when attacking
+            if (isAttacking == true && slowMoveWhileAttacking <= 0.01f)
+            {
+                GetComponent<Rigidbody>().isKinematic = true;
+            }
+
+            playerController.MovePosition(transform.position + (direction * speed * Time.deltaTime * slowMoveWhileAttacking));
         }
     }
 }
